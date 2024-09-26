9ae23ef574b35e271d3b1ab87f9bc946b2d0ad65
Patrickbecerra15 <74045527@iestplurin.edu.pe>
2024-09-26 22:48:17 +0200

    patrick

--- mesapartes/src/main/java/com/mesa/Mesapartes.java
+++ mesapartes/src/main/java/com/mesa/Mesapartes.java
@@ -1,18 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mesa;
 
-/**
- *
- * @author crist
- */
 public class Mesapartes {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("hola");
-         System.out.println("hola soy cristhian ");
+         System.out.println("hola soy tupapi ");
     }
 }
