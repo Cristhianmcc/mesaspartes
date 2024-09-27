18f37478b9bfc4d226a6bd93d6c6ffaa78eec7c0
Cristhianmca <cristhianmc84@gmail.com>
2024-09-26 21:00:10 -0500

    agrenando rama cristhian

--- mesapartes/src/main/java/com/mesa/Mesapartes.java
+++ mesapartes/src/main/java/com/mesa/Mesapartes.java
@@ -13,5 +13,6 @@ public class Mesapartes {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("hola");
+         System.out.println("hola soy cristhian");
     }
 }
